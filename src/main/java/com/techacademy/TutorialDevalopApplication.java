161a0c7a6b230f123cceea10add5976ef2ff5a94
zato0627 <panmimi38@icloud.com>
2025-01-06 11:58:01 +0900

    Lesson18最初のコミット

---
+++ src/main/java/com/techacademy/TutorialDevalopApplication.java
@@ -0,0 +1,13 @@
+package com.techacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialDevalopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialDevalopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techacademy/TutorialDevalopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialDevalopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
